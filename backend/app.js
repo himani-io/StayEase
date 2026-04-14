04be5bc51a6f364540dce53ea04f07490ca2d8e7
Himani <himani.baliyan00@gmail.com>
2026-04-14 14:22:37 +0530

    added a review feature

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,8 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");  
 const wrapAsync = require("./utils/wrapAsync.js");
 const ExpressError = require("./utils/ExpressError.js");
-const {listingSchema} = require("./schema.js")
+const {listingSchema, reviewSchema} = require("./schema.js");
+const Review = require("./models/reviews.js");
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -36,11 +37,25 @@ app.get("/", (req, res) => {
    res.render("listings/index.ejs");
 });
 
-//Server Side Validation
+//Server Side Validation for forms
 const validateListing = (req, res, next) => {
     let {error} = listingSchema.validate(req.body);
     console.log(error);
     if(error) {
+        let errMsg = error.details.map((el) => el.message).join(",");
+        throw new ExpressError(400, errMsg);
+    }else{
+        next();
+    }
+}
+
+
+//Server Side Validation for Reviews
+const validateReview = (req, res, next) => {
+    let {error} = reviewSchema.validate(req.body);
+    // console.log(error.name);
+    if(error) {
+        console.log(error);
         let errMsg = error.details.map((el) => el.message).join(",");
         throw new ExpressError(400, errMsg);
     }else{
@@ -74,7 +89,7 @@ app.post("/listings", validateListing, wrapAsync(async (req, res, next) => {
 //Show route for individual listing READ only
 app.get("/listings/:id",wrapAsync(async(req, res) => {
     let {id} = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("listings/show.ejs" , {listing});
 }));
 
@@ -103,14 +118,40 @@ app.delete("/listings/:id", wrapAsync(async(req, res) => {
     res.redirect("/listings");
 }));
 
+
+//Reviews
+//POST Review Route
+app.post("/listings/:id/reviews", validateReview, wrapAsync(async(req, res) => {
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`);
+}));
+
+
+//Delete Review Route
+app.delete("/listings/:id/reviews/:reviewId", wrapAsync(async(req, res) => {
+   let {id, reviewId} = req.params;
+   await Listing.findByIdAndUpdate(id, {$pull : {reviews: reviewId}});
+   await Review.findByIdAndUpdate(reviewId);
+
+   res.redirect(`/listings/${id}`)
+}))
+
+app.get('/favicon.ico', (req, res) => res.status(204).end());
 app.all(/.*/, (req, res, next) => {
     next(new ExpressError(404, "PAGE not FOUND"));
 })
 
 //Error Handling
 app.use((err, req, res, next) => {
-    console.log(err.name);
+    
     let {statusCode = 500, message = "Something went wrong"} = err;
+    console.log(`Error ${statusCode}: ${message} at ${req.originalUrl}`);
     res.render("listings/error.ejs", {message});
     // res.status(statusCode).send(message)
 });
--- backend/models/listing.js
+++ backend/models/listing.js
@@ -1,18 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const review = require("./reviews.js");
 
 const listingSchema = new Schema({
-    title :{
+    title: {
       type: String,
       required: true,
     },
 
-    description :{
+    description: {
       type: String,
     },
 
     image: {
-  filename: {
+    filename: {
     type: String,
     default: "listingimage",
   },
@@ -27,18 +28,33 @@ const listingSchema = new Schema({
     },
   },
 
-    price :{
+    price: {
       type: Number,
     },
 
-    location :{
+    location: {
       type: String,
     },
 
-    country :{
+    country: {
       type: String,
     },
+
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review", 
+      }
+    
+    ]
 });
+
+listingSchema.post("findOneAndDelete", async(listing) => {
+   if(listing) {
+    await Review.deleteMany({_id: {$in: listing.reviews}})
+   }
+})
+   
 
 const Listing = mongoose.model("Listing", listingSchema);
 module.exports = Listing;
